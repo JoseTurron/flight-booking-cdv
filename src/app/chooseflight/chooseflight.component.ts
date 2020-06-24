ec8e9aaafc77e4c8eb886887f6594b5aaf308c79
JoseTurron <kontakt.rapacz@gmail.com>
2020-06-24 12:58:53 +0200

    Added async and await for flight price API and added basePrice variable to locaStorage

--- src/app/chooseflight/chooseflight.component.ts
+++ src/app/chooseflight/chooseflight.component.ts
@@ -25,6 +25,7 @@ export class ChooseflightComponent implements OnInit {
   public showStorage: any;
   public departureAPI;
   public arrivalAPI;
+  public basePrice: number;
   public today = new Date();
   public todayShort = new Date().toISOString().slice(0,10);
 
@@ -35,31 +36,59 @@ export class ChooseflightComponent implements OnInit {
     { key: "New York", value: ["warsaw, paris,"]}
   ];
 
-  saving() {
+  async getConnection() {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/PL/PLN/en-US/${this.departureAPI}/${this.arrivalAPI}/${this.departureDate}?inboundpartialdate=${this.returnDate}`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host":
+            "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+          "x-rapidapi-key": "4ffdf62c6bmshfb49ff445025abep1e2116jsn7d7aae645a00"
+        }
+      }
+    )
+      .then(response => response.json())
+      .then(data => data.Quotes[0].MinPrice)
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  async saving() {
     if (this.departureAirport == "Warsaw") {
-      this.departureAPI = "WAW-sky"
+      this.departureAPI = "WAW-sky";
     } else if (this.departureAirport == "Paris") {
-      this.departureAPI = "CDG-sky"
+      this.departureAPI = "CDG-sky";
     } else if (this.departureAirport == "New York") {
-      this.departureAPI = "JFK-sky"
+      this.departureAPI = "JFK-sky";
     }
     if (this.destinationAirport == "Warsaw") {
-      this.arrivalAPI = "WAW-sky"
+      this.arrivalAPI = "WAW-sky";
     } else if (this.destinationAirport == "Paris") {
-      this.arrivalAPI = "CDG-sky"
+      this.arrivalAPI = "CDG-sky";
     } else if (this.destinationAirport == "New York") {
-      this.arrivalAPI = "JFK-sky"
+      this.arrivalAPI = "JFK-sky";
     }
-    let dataStorage = {
-      departureDate: this.departureDate,
-      returnDate: this.returnDate,
-      departureAirport: this.departureAirport,
-      arrivalAirport: this.destinationAirport,
-      passengersNumber: this.numberOfPassengers,
-      departureAPI: this.departureAPI,
-      arrivalAPI: this.arrivalAPI
-    };
-    localStorage.setItem("flightdetails", JSON.stringify(dataStorage));
-    this.showStorage = JSON.parse(localStorage.getItem("flightdetails"));
+
+    try {
+      this.basePrice = await this.getConnection();
+
+      let dataStorage = {
+        departureDate: this.departureDate,
+        returnDate: this.returnDate,
+        departureAirport: this.departureAirport,
+        arrivalAirport: this.destinationAirport,
+        passengersNumber: this.numberOfPassengers,
+        departureAPI: this.departureAPI,
+        arrivalAPI: this.arrivalAPI,
+        basePrice: this.basePrice
+      };
+
+      localStorage.setItem("flightdetails", JSON.stringify(dataStorage));
+      this.showStorage = JSON.parse(localStorage.getItem("flightdetails"));
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
